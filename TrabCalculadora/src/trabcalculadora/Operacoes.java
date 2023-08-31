f54ce0ee4fa4d3875f216b7c7b3721caffc78086
Felipe Sutter <lipeps61@gmail.com>
2023-08-31 20:06:25 -0300

    Trab de calculadora em Java

---
+++ TrabCalculadora/src/trabcalculadora/Operacoes.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package trabcalculadora;
+
+/**
+ *
+ * @author fsutter_faeterj-petr
+ */
+public class Operacoes {
+    
+    public static int soma(int valor1, int valor2) {
+        return valor1 + valor2;
+    }
+    
+    public static int subtracao(int valor1, int valor2) {
+        return valor1 - valor2;
+    }
+    
+    public static int multiplicacao(int valor1, int valor2) {
+        return valor1 * valor2;
+    }
+    
+    public static int divisao(int valor1, int valor2) {
+        return valor1 / valor2;
+    }
+}
